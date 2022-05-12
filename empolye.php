d251b4774714b1f9b51fad657f0c6e18b40a8bf0
Mahima1111 <mahimavijayan123@gmail.com>
2022-05-12 13:47:04 +0530

    web application completed

---
+++ empolye.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table class="table table-borderless">
+        <tr>
+            <td>Emp code</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>Name</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>Designation</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>Salary</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>Date of Birth</td>
+            <td><input class="form-control" type="date" name="" id=""></td>
+        </tr>
+        <tr>
+            <td>Email Id</td>
+            <td><input class="form-control" type="email" name="" id=""></td>
+        </tr>
+        <tr>
+            <td>Age</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>Phone Number</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td><textarea class="form-control" name="" id="" cols="30" rows="5"></textarea></td>
+        </tr>
+        <tr>
+            <td>Pincode</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>User Name</td>
+            <td><input class="form-control" type="text"></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><input class="form-control" type="password" name="" id=""></td>
+        </tr>
+        <tr>
+            <td>Confirm Password</td>
+            <td><input class="form-control" type="password" name="" id=""></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><BUtton class="btn btn-success" >REGISTER</BUtton> <button btn class="btn btn-dark">RESET</button></td>
+        </tr>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ faculty.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faculty</title>
+</head>
+<body>
+    <table class="table table-borderless">
+        <tr>
+            <td> Department</td>
+            <td> <select name="" id="" class="form-control">
+                <option value="MCA">MCA</option>
+                <option value="MBA">MBA</option>
+                <option value="MCOM">MCOM</option>
+            </select></td> ></td>
+        </tr>
+        <tr>
+            <td>NAME</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td>Educational Qualification</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td>Address</td>
+            <td><textarea name="" id="" cols="30" rows="4" class="form-control"></textarea></td>
+        </tr>
+        <tr>
+            <td>Pincode</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td>District</td>
+            <td><select name="" id="" class="form-control">
+                <option value="TRIVANDRUM">TVM</option>
+                <option value="KOLLAM">KOLLAM</option>
+                <option value="PATHNAMTHITTA">PATHNAMTHITTA</option>
+                <option value="ALAPUZHA">ALAPUZHA</option>
+                <option value="KOTTAYAM">KOTTAYAM</option>
+                <option value="IDUKKI">IDUKKI</option>
+                <option value="ERNNAKULAM">ERNNAKULAM</option>
+                <option value="THISSUR">THISSUR</option>
+                <option value="PALAKAD">PALAKAD</option>
+                <option value="MALAPPURAM">MALAPPURAM</option>
+                <option value="KOZHIKKODE">KOZHIKKODE</option>
+                <option value="WAYANAD">WAYANAD</option>
+                <option value="KANNUR">KANNUR</option>
+                <option value="KASRGODE">KASRGODE</option>
+            </select></td>
+        </tr>
+        <tr>
+            <td>Email Id</td>
+            <td><input type="email" name="" id="" class="form-control"></td>
+        </tr>
+        <tr>
+            <td>Phone number</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td>Gender</td>
+            <td><select name="" id="" class="form-control">
+                <option value="Female">Female</option>
+                <option value="Male">Male</option>
+                <option value="Other">Other</option>
+            </select></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td><input type="password" name="" id="" class="form-control"></td>
+        </tr>
+        <tr>
+            <td>Confirm Password</td>
+            <td><input type="password" name="" id="" class="form-control"></td>
+        </tr>
+        <tr>
+            <td><input type="checkbox" name="" id="">Agree Terms&Condition</td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><button class="btn btn-danger">Register</button></td>
+        </tr>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
